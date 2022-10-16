584be5e97c2f8dd24c2ac62e3f077e7fd26165a4
leo <leonardo.s.deluca@gmail.com>
2022-10-16 15:17:21 -0300

    Inicio Qrcode

--- lib/componentes/qrCodeMotorista.dart
+++ lib/componentes/qrCodeMotorista.dart
@@ -1,11 +1,59 @@
 import 'package:flutter/src/widgets/container.dart';
 import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_barcode_scanner/flutter_barcode_scanner.dart';
 
 class qrCodeMotorista extends StatelessWidget {
   const qrCodeMotorista({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return MaterialApp(
+      title: 'QRCODE',
+      home: QRCodePage(),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
+
+class QRCodePage extends StatefulWidget {
+  QRCodePage({Key? key}) : super(key: key);
+
+  @override
+  _QRCodePageState createState() => _QRCodePageState();
+}
+
+class _QRCodePageState extends State<QRCodePage> {
+  String ticket = '';
+
+  readQRCode() async {
+    String code = await FlutterBarcodeScanner.scanBarcode(
+        "#FFFFFF", "Cancelar", true, ScanMode.QR);
+    setState(() => ticket = code != '-1' ? code : 'Não Validado');
+  }
+
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: Container(
+            width: MediaQuery.of(context).size.width,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                if (ticket != '')
+                  Padding(
+                    padding: EdgeInsets.only(bottom: 24.0),
+                    child: Text(
+                      'Ticket: $ticket',
+                      style: TextStyle(fontSize: 20),
+                    ),
+                  ),
+                ElevatedButton.icon(
+                  onPressed: () => readQRCode(),
+                  icon: Icon(Icons.qr_code),
+                  label: Text('Validar'),
+                )
+              ],
+            )));
   }
 }
